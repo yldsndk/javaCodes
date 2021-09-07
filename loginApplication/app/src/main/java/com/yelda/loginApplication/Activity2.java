94bcc866d06c42e06a4b86602a05c0931b09cfa9
yldsndk <yeldasandikci@gmail.com>
2021-09-07 13:07:32 +0300

    Applications - Android Studio

---
+++ loginApplication/app/src/main/java/com/yelda/loginApplication/Activity2.java
@@ -0,0 +1,12 @@
+package com.yelda.loginApplication;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Activity2 extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity2);
+    }
+}
